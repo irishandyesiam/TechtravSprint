a3fe19d9b338ffe54e4cb83f140e5f025a90af8e
irishandyesiam <nathan.irish@gmail.com>
2023-03-18 13:27:10 -0500

    CRUD url endpoints or routes set and tested.

--- Cookbook/routes/api.php
+++ Cookbook/routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\Api\UserController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -14,6 +15,17 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+// Route::apiResource('users', UserController::class);
+
+Route::prefix('users')
+->name('users.')
+->group(function () {
+    Route::get('/', [userController::class, 'index'])->name('index');
+    Route::post('/', [userController::class, 'store'])->name('store');
+    Route::get('/', [userController::class, 'show'])->name('show');
+    Route::put('/', [userController::class, 'update'])->name('update');
+    Route::delete('/', [userController::class, 'destory'])->name('destory');
+});
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
