0f959da9bb9ed115fd31eebd551f39221455a015
irishandyesiam <nathan.irish@gmail.com>
2023-03-19 12:33:52 -0500

    openssl s-client -connect working, but response is 'No such host is known'.

--- Cookbook/app/Http/Controllers/TastyController.php
+++ Cookbook/app/Http/Controllers/TastyController.php
@@ -7,8 +7,15 @@ use Illuminate\Support\Facades\Http;
 
 class TastyController extends Controller
 {
-    
-    // The remote server's SSL certificate or SSH fingerprint was deemed not OK. This error code has been unified with CURLE_SSL_CACERT since 7.62.0. Its previous value was 51. Installed OpenSSL, Strawberry, and NASM. I set up all PATH. I still cannot build openSSL to retrieve SSL certficate and check if valid.
+    /*The remote server's SSL certificate or SSH fingerprint was deemed not OK.
+    This error code has been unified with CURLE_SSL_CACERT since 7.62.0.
+    Its previous value was 51.Installed OpenSSL, Strawberry Perl, NASM and set up PATHs 
+    to check if remote server's SSL certificate is valid. cmd prompts still not recognized.*/
+
+    /*No such host is know? 
+        PS C:\Users\User\Desktop\TechtravSprint\TechtravSprint\Cookbook> openssl s_client -connect tasty.p.rapidapi.com.443
+        A43D0000:error:10080002:BIO routines:BIO_lookup_ex:system lib:crypto\bio\bio_addr.c:738:No such host is known. 
+        connect:errno=11001*/
     public function index()
     {
         $response = Http::withHeaders([
