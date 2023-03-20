cddf50035b426d87b078c0ab7b113d2c7b82f0b2
irishandyesiam <nathan.irish@gmail.com>
2023-03-19 19:20:40 -0500

    Update and Delete button color corrected.

--- Cookbook/resources/views/recipes/index.blade.php
+++ Cookbook/resources/views/recipes/index.blade.php
@@ -11,29 +11,33 @@
                 <div class="py-12">
                     <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
                         <div class ="py-4 px-4 bg-white overflow-hidden shadow-sm sm:rounded-lg">
-                            <table class="table table-bordered">
-                                <thead>
-                                    <tr>
-                                        <th>Recipe Name</th>
-                                        <th>Image</th>
-                                        <th>Ingredients</th>
-                                        <th>Instructions</th>
-                                        <th>Update</th>
-                                        <th>Delete</th>
+                            <table class="min-w-full border text-center text-sm font-light dark:border-neutral-500">
+                                <thead class="border-b font-medium dark:border-neutral-500">
+                                    <tr >
+                                        <th scope="col" class="px-6 py-4">Recipe Name</th>
+                                        <th scope="col" class="px-6 py-4">Image</th>
+                                        <th scope="col" class="px-6 py-4">Ingredients</th>
+                                        <th scope="col" class="px-6 py-4">Instructions</th>
+                                        <th scope="col" class="px-6 py-4">Update</th>
+                                        <th scope="col" class="px-6 py-4">Delete</th>
                                     </tr>
                                 </thead>
                                 <tbody>
                                     @forelse ($recipes as $recipe)
-                                    <tr>
-                                        <td>{{ $recipe->recipeName }}</td>
-                                        <td>{{ $recipe->image }}</td>
-                                        <td>{{ $recipe->ingredients }}</td>
-                                        <td>{{ $recipe->instructions }}</td>
-                                        <td>
-                                            <a href="{{ url('/') }}" class="btn btn-primary">Update</a>
+                                    <tr class="border-b font-medium dark:border-neutral-500">
+                                        <td class="whitespace-nowrap px-6 py-4">{{ $recipe->recipeName }}</td>
+                                        <td class="whitespace-nowrap px-6 py-4">{{ $recipe->image }}</td>
+                                        <td class="whitespace-nowrap px-6 py-4">{{ $recipe->ingredients }}</td>
+                                        <td class="whitespace-nowrap px-6 py-4">{{ $recipe->instructions }}</td>
+                                        <td class="whitespace-nowrap px-6 py-4">
+                                            <a href="{{ url('/') }}">
+                                                <x-primary-button class="mt-4">{{ __('Update') }}</x-primary-button>
+                                            </a>
                                         </td>
-                                        <td>
-                                            <a href="{{ url('/') }}" class="btn btn-danger">Delete</a>
+                                        <td class="whitespace-nowrap px-6 py-4">
+                                            <a href="{{ url('/') }}">
+                                                <x-danger-button class="mt-4">{{ __('Delete') }}</x-danger-button>
+                                            </a>
                                         </td>
                                     </tr>
                                     @empty
